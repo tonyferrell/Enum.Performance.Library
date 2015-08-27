213f92e7e0114da4beaca17f8e903e55c8baab40
Tony Ferrell <tony@codeawhile.com>
2015-08-27 01:08:58 -0700

    Add examples

---
+++ Examples/Examples/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var example = new EnumManagerExample();
+            Console.WriteLine("Get Results: " + example.GetResults(args));
+            Console.WriteLine("Get More Results: " + example.GetMoreResults(args));
+            Debugger.Break();
+        }
+    }
+}
